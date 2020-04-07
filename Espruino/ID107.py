275d55d34d8e1637d8f88af9b98774beb3f66fab
abhigkar <abhinav.golwalkar@gmail.com>
2020-04-07 20:34:56 +0100

    Update ID107.py

--- Espruino/ID107.py
+++ Espruino/ID107.py
@@ -93,11 +93,6 @@ def get_pins():
   pins = pinutils.generate_pins(0,31) # 32 General Purpose I/O Pins.
   pinutils.findpin(pins, "PD0", True)["functions"]["XL1"]=0;
   pinutils.findpin(pins, "PD1", True)["functions"]["XL2"]=0;
-  pinutils.findpin(pins, "PD5", True)["functions"]["RTS"]=0;
-  pinutils.findpin(pins, "PD6", True)["functions"]["TXD"]=0;
-  pinutils.findpin(pins, "PD7", True)["functions"]["CTS"]=0;
-  pinutils.findpin(pins, "PD8", True)["functions"]["RXD"]=0;
-  pinutils.findpin(pins, "PD9", True)["functions"]["NFC1"]=0;
   pinutils.findpin(pins, "PD10", True)["functions"]["NFC2"]=0;
   pinutils.findpin(pins, "PD2", True)["functions"]["ADC1_IN0"]=0;
   pinutils.findpin(pins, "PD3", True)["functions"]["ADC1_IN1"]=0;
@@ -107,15 +102,6 @@ def get_pins():
   pinutils.findpin(pins, "PD29", True)["functions"]["ADC1_IN5"]=0;
   pinutils.findpin(pins, "PD30", True)["functions"]["ADC1_IN6"]=0;
   pinutils.findpin(pins, "PD31", True)["functions"]["ADC1_IN7"]=0;
-  # Make buttons and LEDs negated
-  pinutils.findpin(pins, "PD13", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD14", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD15", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD16", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD17", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD18", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD19", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD20", True)["functions"]["NEGATED"]=0;
 
   # everything is non-5v tolerant
   for pin in pins:
